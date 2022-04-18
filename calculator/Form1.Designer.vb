ebf9c7a46e0e969e88d04868a1ce05757becf8b7
austi <austi@LAPTOP-HH3B90IE>
2022-04-18 11:38:48 +0800

    加入專案檔案。

---
+++ calculator/Form1.Designer.vb
@@ -0,0 +1,277 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.LB_num1 = New System.Windows.Forms.Label()
+        Me.TB_show = New System.Windows.Forms.TextBox()
+        Me.BTN_num1 = New System.Windows.Forms.Button()
+        Me.BTN_num2 = New System.Windows.Forms.Button()
+        Me.BTN_num3 = New System.Windows.Forms.Button()
+        Me.BTN_num4 = New System.Windows.Forms.Button()
+        Me.BTN_num5 = New System.Windows.Forms.Button()
+        Me.BTN_num6 = New System.Windows.Forms.Button()
+        Me.BTN_num7 = New System.Windows.Forms.Button()
+        Me.BTN_num8 = New System.Windows.Forms.Button()
+        Me.BTN_num9 = New System.Windows.Forms.Button()
+        Me.BTN_num0 = New System.Windows.Forms.Button()
+        Me.BTN_plus = New System.Windows.Forms.Button()
+        Me.BTN_minus = New System.Windows.Forms.Button()
+        Me.BTN_multiply = New System.Windows.Forms.Button()
+        Me.BTN_divide = New System.Windows.Forms.Button()
+        Me.BTN_dot = New System.Windows.Forms.Button()
+        Me.BTN_equal = New System.Windows.Forms.Button()
+        Me.BTN_clean = New System.Windows.Forms.Button()
+        Me.BTN_cancel = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'LB_num1
+        '
+        Me.LB_num1.AutoSize = True
+        Me.LB_num1.Location = New System.Drawing.Point(12, 9)
+        Me.LB_num1.Name = "LB_num1"
+        Me.LB_num1.Size = New System.Drawing.Size(0, 15)
+        Me.LB_num1.TabIndex = 0
+        '
+        'TB_show
+        '
+        Me.TB_show.Location = New System.Drawing.Point(12, 34)
+        Me.TB_show.Name = "TB_show"
+        Me.TB_show.Size = New System.Drawing.Size(292, 23)
+        Me.TB_show.TabIndex = 1
+        '
+        'BTN_num1
+        '
+        Me.BTN_num1.Location = New System.Drawing.Point(12, 63)
+        Me.BTN_num1.Name = "BTN_num1"
+        Me.BTN_num1.Size = New System.Drawing.Size(75, 23)
+        Me.BTN_num1.TabIndex = 2
+        Me.BTN_num1.Text = "1"
+        Me.BTN_num1.UseVisualStyleBackColor = True
+        '
+        'BTN_num2
+        '
+        Me.BTN_num2.Location = New System.Drawing.Point(12, 92)
+        Me.BTN_num2.Name = "BTN_num2"
+        Me.BTN_num2.Size = New System.Drawing.Size(75, 23)
+        Me.BTN_num2.TabIndex = 3
+        Me.BTN_num2.Text = "2"
+        Me.BTN_num2.UseVisualStyleBackColor = True
+        '
+        'BTN_num3
+        '
+        Me.BTN_num3.Location = New System.Drawing.Point(12, 121)
+        Me.BTN_num3.Name = "BTN_num3"
+        Me.BTN_num3.Size = New System.Drawing.Size(75, 23)
+        Me.BTN_num3.TabIndex = 4
+        Me.BTN_num3.Text = "3"
+        Me.BTN_num3.UseVisualStyleBackColor = True
+        '
+        'BTN_num4
+        '
+        Me.BTN_num4.Location = New System.Drawing.Point(12, 150)
+        Me.BTN_num4.Name = "BTN_num4"
+        Me.BTN_num4.Size = New System.Drawing.Size(75, 23)
+        Me.BTN_num4.TabIndex = 5
+        Me.BTN_num4.Text = "4"
+        Me.BTN_num4.UseVisualStyleBackColor = True
+        '
+        'BTN_num5
+        '
+        Me.BTN_num5.Location = New System.Drawing.Point(12, 179)
+        Me.BTN_num5.Name = "BTN_num5"
+        Me.BTN_num5.Size = New System.Drawing.Size(75, 23)
+        Me.BTN_num5.TabIndex = 6
+        Me.BTN_num5.Text = "5"
+        Me.BTN_num5.UseVisualStyleBackColor = True
+        '
+        'BTN_num6
+        '
+        Me.BTN_num6.Location = New System.Drawing.Point(93, 63)
+        Me.BTN_num6.Name = "BTN_num6"
+        Me.BTN_num6.Size = New System.Drawing.Size(75, 23)
+        Me.BTN_num6.TabIndex = 7
+        Me.BTN_num6.Text = "6"
+        Me.BTN_num6.UseVisualStyleBackColor = True
+        '
+        'BTN_num7
+        '
+        Me.BTN_num7.Location = New System.Drawing.Point(93, 92)
+        Me.BTN_num7.Name = "BTN_num7"
+        Me.BTN_num7.Size = New System.Drawing.Size(75, 23)
+        Me.BTN_num7.TabIndex = 8
+        Me.BTN_num7.Text = "7"
+        Me.BTN_num7.UseVisualStyleBackColor = True
+        '
+        'BTN_num8
+        '
+        Me.BTN_num8.Location = New System.Drawing.Point(93, 121)
+        Me.BTN_num8.Name = "BTN_num8"
+        Me.BTN_num8.Size = New System.Drawing.Size(75, 23)
+        Me.BTN_num8.TabIndex = 9
+        Me.BTN_num8.Text = "8"
+        Me.BTN_num8.UseVisualStyleBackColor = True
+        '
+        'BTN_num9
+        '
+        Me.BTN_num9.Location = New System.Drawing.Point(93, 150)
+        Me.BTN_num9.Name = "BTN_num9"
+        Me.BTN_num9.Size = New System.Drawing.Size(75, 23)
+        Me.BTN_num9.TabIndex = 10
+        Me.BTN_num9.Text = "9"
+        Me.BTN_num9.UseVisualStyleBackColor = True
+        '
+        'BTN_num0
+        '
+        Me.BTN_num0.Location = New System.Drawing.Point(93, 179)
+        Me.BTN_num0.Name = "BTN_num0"
+        Me.BTN_num0.Size = New System.Drawing.Size(75, 23)
+        Me.BTN_num0.TabIndex = 11
+        Me.BTN_num0.Text = "0"
+        Me.BTN_num0.UseVisualStyleBackColor = True
+        '
+        'BTN_plus
+        '
+        Me.BTN_plus.Location = New System.Drawing.Point(174, 63)
+        Me.BTN_plus.Name = "BTN_plus"
+        Me.BTN_plus.Size = New System.Drawing.Size(75, 23)
+        Me.BTN_plus.TabIndex = 12
+        Me.BTN_plus.Text = "+"
+        Me.BTN_plus.UseVisualStyleBackColor = True
+        '
+        'BTN_minus
+        '
+        Me.BTN_minus.Location = New System.Drawing.Point(174, 92)
+        Me.BTN_minus.Name = "BTN_minus"
+        Me.BTN_minus.Size = New System.Drawing.Size(75, 23)
+        Me.BTN_minus.TabIndex = 13
+        Me.BTN_minus.Text = "-"
+        Me.BTN_minus.UseVisualStyleBackColor = True
+        '
+        'BTN_multiply
+        '
+        Me.BTN_multiply.Location = New System.Drawing.Point(174, 121)
+        Me.BTN_multiply.Name = "BTN_multiply"
+        Me.BTN_multiply.Size = New System.Drawing.Size(75, 23)
+        Me.BTN_multiply.TabIndex = 14
+        Me.BTN_multiply.Text = "*"
+        Me.BTN_multiply.UseVisualStyleBackColor = True
+        '
+        'BTN_divide
+        '
+        Me.BTN_divide.Location = New System.Drawing.Point(174, 150)
+        Me.BTN_divide.Name = "BTN_divide"
+        Me.BTN_divide.Size = New System.Drawing.Size(75, 23)
+        Me.BTN_divide.TabIndex = 15
+        Me.BTN_divide.Text = "/"
+        Me.BTN_divide.UseVisualStyleBackColor = True
+        '
+        'BTN_dot
+        '
+        Me.BTN_dot.Location = New System.Drawing.Point(174, 179)
+        Me.BTN_dot.Name = "BTN_dot"
+        Me.BTN_dot.Size = New System.Drawing.Size(75, 23)
+        Me.BTN_dot.TabIndex = 16
+        Me.BTN_dot.Text = "."
+        Me.BTN_dot.UseVisualStyleBackColor = True
+        '
+        'BTN_equal
+        '
+        Me.BTN_equal.Location = New System.Drawing.Point(255, 63)
+        Me.BTN_equal.Name = "BTN_equal"
+        Me.BTN_equal.Size = New System.Drawing.Size(75, 23)
+        Me.BTN_equal.TabIndex = 17
+        Me.BTN_equal.Text = "="
+        Me.BTN_equal.UseVisualStyleBackColor = True
+        '
+        'BTN_clean
+        '
+        Me.BTN_clean.Location = New System.Drawing.Point(255, 92)
+        Me.BTN_clean.Name = "BTN_clean"
+        Me.BTN_clean.Size = New System.Drawing.Size(75, 23)
+        Me.BTN_clean.TabIndex = 18
+        Me.BTN_clean.Text = "C"
+        Me.BTN_clean.UseVisualStyleBackColor = True
+        '
+        'BTN_cancel
+        '
+        Me.BTN_cancel.Location = New System.Drawing.Point(255, 121)
+        Me.BTN_cancel.Name = "BTN_cancel"
+        Me.BTN_cancel.Size = New System.Drawing.Size(75, 23)
+        Me.BTN_cancel.TabIndex = 19
+        Me.BTN_cancel.Text = "AC"
+        Me.BTN_cancel.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(385, 223)
+        Me.Controls.Add(Me.BTN_cancel)
+        Me.Controls.Add(Me.BTN_clean)
+        Me.Controls.Add(Me.BTN_equal)
+        Me.Controls.Add(Me.BTN_dot)
+        Me.Controls.Add(Me.BTN_divide)
+        Me.Controls.Add(Me.BTN_multiply)
+        Me.Controls.Add(Me.BTN_minus)
+        Me.Controls.Add(Me.BTN_plus)
+        Me.Controls.Add(Me.BTN_num0)
+        Me.Controls.Add(Me.BTN_num9)
+        Me.Controls.Add(Me.BTN_num8)
+        Me.Controls.Add(Me.BTN_num7)
+        Me.Controls.Add(Me.BTN_num6)
+        Me.Controls.Add(Me.BTN_num5)
+        Me.Controls.Add(Me.BTN_num4)
+        Me.Controls.Add(Me.BTN_num3)
+        Me.Controls.Add(Me.BTN_num2)
+        Me.Controls.Add(Me.BTN_num1)
+        Me.Controls.Add(Me.TB_show)
+        Me.Controls.Add(Me.LB_num1)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents LB_num1 As Label
+    Friend WithEvents TB_show As TextBox
+    Friend WithEvents BTN_num1 As Button
+    Friend WithEvents BTN_num2 As Button
+    Friend WithEvents BTN_num3 As Button
+    Friend WithEvents BTN_num4 As Button
+    Friend WithEvents BTN_num5 As Button
+    Friend WithEvents BTN_num6 As Button
+    Friend WithEvents BTN_num7 As Button
+    Friend WithEvents BTN_num8 As Button
+    Friend WithEvents BTN_num9 As Button
+    Friend WithEvents BTN_num0 As Button
+    Friend WithEvents BTN_plus As Button
+    Friend WithEvents BTN_minus As Button
+    Friend WithEvents BTN_multiply As Button
+    Friend WithEvents BTN_divide As Button
+    Friend WithEvents BTN_dot As Button
+    Friend WithEvents BTN_equal As Button
+    Friend WithEvents BTN_clean As Button
+    Friend WithEvents BTN_cancel As Button
+End Class
---
+++ calculator/Form1.vb
@@ -0,0 +1,211 @@
+﻿Public Class Form1
+    Dim num1, num2 As Double
+    Dim count As String
+
+    Private Sub BTN_num1_Click(sender As Object, e As EventArgs) Handles BTN_num1.Click
+        TB_show.Text &= "1"
+    End Sub
+
+    Private Sub BTN_num2_Click(sender As Object, e As EventArgs) Handles BTN_num2.Click
+        TB_show.Text &= "2"
+    End Sub
+
+    Private Sub BTN_num3_Click(sender As Object, e As EventArgs) Handles BTN_num3.Click
+        TB_show.Text &= "3"
+    End Sub
+
+    Private Sub BTN_num4_Click(sender As Object, e As EventArgs) Handles BTN_num4.Click
+        TB_show.Text &= "4"
+    End Sub
+
+    Private Sub BTN_num5_Click(sender As Object, e As EventArgs) Handles BTN_num5.Click
+        TB_show.Text &= "5"
+    End Sub
+
+    Private Sub BTN_num6_Click(sender As Object, e As EventArgs) Handles BTN_num6.Click
+        TB_show.Text &= "6"
+    End Sub
+
+    Private Sub BTN_num7_Click(sender As Object, e As EventArgs) Handles BTN_num7.Click
+        TB_show.Text &= "7"
+    End Sub
+
+    Private Sub BTN_num8_Click(sender As Object, e As EventArgs) Handles BTN_num8.Click
+        TB_show.Text &= "8"
+    End Sub
+
+    Private Sub BTN_num9_Click(sender As Object, e As EventArgs) Handles BTN_num9.Click
+        TB_show.Text &= "9"
+    End Sub
+
+    Private Sub BTN_num0_Click(sender As Object, e As EventArgs) Handles BTN_num0.Click
+        If TB_show.Text = "" Then
+            TB_show.Text = "0."
+        Else
+            TB_show.Text &= "0"
+        End If
+    End Sub
+
+    Private Sub BTN_dot_Click(sender As Object, e As EventArgs) Handles BTN_dot.Click
+        If TB_show.Text.Contains(".") Then
+            Return
+        Else
+            TB_show.Text &= "."
+        End If
+    End Sub
+
+    '清除
+    Private Sub BTN_clean_Click(sender As Object, e As EventArgs) Handles BTN_clean.Click
+        TB_show.Text = ""
+    End Sub
+
+    '取消
+    Private Sub BTN_cancel_Click(sender As Object, e As EventArgs) Handles BTN_cancel.Click
+        num1 = 0
+        num2 = 0
+        count = ""
+        TB_show.Text = ""
+        LB_num1.Text = ""
+    End Sub
+
+    '加
+    Private Sub BTN_plus_Click(sender As Object, e As EventArgs) Handles BTN_plus.Click
+        If CHK_Textbox(Me.TB_show) Then
+            If LB_num1.Text.Contains("+") Then '直接按+多次
+                num2 = Convert.ToDouble(TB_show.Text) '輸入的第二數字
+                GoMath(num1, num2, "+")
+
+            ElseIf LB_num1.Text.Contains("=") Then '前一次按=
+                num1 = LB_num1.Text.Split("=")(1) '前次結果為第一數字
+                num2 = Convert.ToDouble(TB_show.Text) '輸入的第二數字
+                GoMath(num1, num2, "+")
+
+            Else  '第一次按+
+                num1 = Convert.ToDouble(TB_show.Text)
+                count = BTN_plus.Text
+                LB_num1.Text = num1.ToString & count
+                TB_show.Text = ""
+            End If
+        End If
+    End Sub
+
+    '減
+    Private Sub BTN_minus_Click(sender As Object, e As EventArgs) Handles BTN_minus.Click
+        If CHK_Textbox(Me.TB_show) Then
+            If LB_num1.Text.Contains("-") Then '直接按-多次
+                num2 = Convert.ToDouble(TB_show.Text) '輸入的第二數字
+                GoMath(num1, num2, "-")
+
+            ElseIf LB_num1.Text.Contains("=") Then '前一次按=
+                num1 = LB_num1.Text.Split("=")(1) '前次結果為第一數字
+                num2 = Convert.ToDouble(TB_show.Text) '輸入的第二數字
+                GoMath(num1, num2, "-")
+
+            Else  '第一次按-
+                num1 = Convert.ToDouble(TB_show.Text)
+                count = BTN_minus.Text
+                LB_num1.Text = num1.ToString & count
+                TB_show.Text = ""
+            End If
+        End If
+    End Sub
+
+    '乘
+    Private Sub BTN_multiply_Click(sender As Object, e As EventArgs) Handles BTN_multiply.Click
+        If CHK_Textbox(Me.TB_show) Then
+            If LB_num1.Text.Contains("*") Then '直接按*多次
+                num2 = Convert.ToDouble(TB_show.Text) '輸入的第二數字
+                GoMath(num1, num2, "*")
+
+            ElseIf LB_num1.Text.Contains("=") Then '前一次按=
+                num1 = LB_num1.Text.Split("=")(1) '前次結果為第一數字
+                num2 = Convert.ToDouble(TB_show.Text) '輸入的第二數字
+                GoMath(num1, num2, "*")
+
+            Else  '第一次按-
+                num1 = Convert.ToDouble(TB_show.Text)
+                count = BTN_multiply.Text
+                LB_num1.Text = num1.ToString & count
+                TB_show.Text = ""
+            End If
+        End If
+    End Sub
+
+    '除
+    Private Sub BTN_divide_Click(sender As Object, e As EventArgs) Handles BTN_divide.Click
+        'If TB_show.Text.Contains(".") AndAlso TB_show.Text.Split(".")(1).ToString = "" Then '檢查輸入小數點
+        '    count = ""
+        '    MsgBox("請輸入有效數字")
+        '    Exit Sub
+        'ElseIf TB_show.Text = "" Then
+        '    MsgBox("請輸入有效數字")
+        '    count = ""
+        '    Exit Sub
+        'End If
+
+        If CHK_Textbox(Me.TB_show) Then
+            If LB_num1.Text.Contains("/") Then '直接按*多次
+                num2 = Convert.ToDouble(TB_show.Text) '輸入的第二數字
+                GoMath(num1, num2, "/")
+
+            ElseIf LB_num1.Text.Contains("=") Then '前一次按=
+                num1 = LB_num1.Text.Split("=")(1) '前次結果為第一數字
+                num2 = Convert.ToDouble(TB_show.Text) '輸入的第二數字
+                GoMath(num1, num2, "/")
+
+            Else  '第一次按-
+                num1 = Convert.ToDouble(TB_show.Text)
+                count = BTN_divide.Text
+                LB_num1.Text = num1.ToString & count
+                TB_show.Text = ""
+            End If
+        End If
+    End Sub
+
+    '等於
+    Private Sub BTN_equal_Click(sender As Object, e As EventArgs) Handles BTN_equal.Click
+        If LB_num1.Text <> "" AndAlso TB_show.Text <> "" Then
+            If Not LB_num1.Text.Contains("=") Then  '尚未按過=
+                num2 = Convert.ToDouble(TB_show.Text) '輸入放入第二數子
+                GoMath(num1, num2, count) '計算結果顯示
+            End If
+        End If
+    End Sub
+
+    '檢查輸入值
+    Private Function CHK_Textbox(TB As TextBox) As Boolean
+        If TB.Text.Contains(".") AndAlso TB.Text.Split(".")(1).ToString = "" Then
+            count = ""
+            MsgBox("請輸入有效數字")
+            Return False
+        ElseIf TB.Text = "" Then
+            MsgBox("請輸入有效數字")
+            count = ""
+            Return False
+        End If
+
+        Return True
+    End Function
+
+    '計算
+    Private Sub GoMath(val1 As Double, val2 As Double, cnt As String)
+        Select Case cnt
+            Case "+"
+                LB_num1.Text = val1 & "+" & val2 & "=" & val1 + val2
+                TB_show.Text = ""
+                num1 = val1 + val2
+            Case "-"
+                LB_num1.Text = val1 & "-" & val2 & "=" & val1 - val2
+                TB_show.Text = ""
+                num1 = val1 - val2
+            Case "*"
+                LB_num1.Text = val1 & "*" & val2 & "=" & val1 * val2
+                TB_show.Text = ""
+                num1 = val1 * val2
+            Case "/"
+                LB_num1.Text = val1 & "/" & val2 & "=" & val1 / val2
+                TB_show.Text = ""
+                num1 = val1 / val2
+        End Select
+    End Sub
+End Class
